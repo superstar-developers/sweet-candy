51b072d219ed2b298b010b7b6b621312838aee88
ventureful <hades0122@outlook.com>
2023-06-23 05:27:19 +0200

    frontend boilerplate

---
+++ frontend-angular13/src/app/pages/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  username: string = "";
+  password: string = "";
+  show: boolean = false;
+  submit() {
+    console.log("user name is " + this.username)
+    this.clear();
+  }
+  clear() {
+    this.username = "";
+    this.password = "";
+    this.show = true;
+  }
+  onLogin(){
+    console.log('login');
+
+  }
+
+}
